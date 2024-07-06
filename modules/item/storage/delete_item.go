8341dd83fe801886bb1545590bf22b343b60a498
khiemhome <khiembi161219@gmail.com>
2024-07-06 09:46:28 +0700

    API Delete Item

---
+++ modules/item/storage/delete_item.go
@@ -0,0 +1,16 @@
+package storage
+
+import (
+	"Learn_Golang/modules/item/model"
+	"context"
+)
+
+func (sql *sqlStore) DeleteItem(ctx context.Context, cond map[string]interface{}) error {
+
+	if err := sql.db.Table(model.TodoItem{}.TableName()).Where(cond).Updates(map[string]interface{}{
+		"status": "Deleted",
+	}); err.Error != nil {
+		return err.Error
+	}
+	return nil
+}
