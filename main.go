1501bd7831871f23c8b77dab13db7834813e31d6
khiemhome <khiembi161219@gmail.com>
2024-07-17 10:51:10 +0700

    Package Scope

--- main.go
+++ main.go
@@ -1,36 +1,45 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, value := range names {
-		initials = append(initials, value[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-}
+var score = 100
 
 func main() {
-	fn1, sn1 := getInitials("tifa lockhart")
-	fmt.Println(fn1, sn1)
+	sayHello("mario")
 
-	fn2, sn2 := getInitials("cloud strife")
-	fmt.Println(fn2, sn2)
+	for _, v := range points {
+		fmt.Println(v)
+	}
 
-	fn3, sn3 := getInitials("vincent")
-	fmt.Println(fn3, sn3)
+	showScore()
 }
+
+//func getInitials(n string) (string, string) {
+//	s := strings.ToUpper(n)
+//	names := strings.Split(s, " ")
+//
+//	var initials []string
+//	for _, value := range names {
+//		initials = append(initials, value[:1])
+//	}
+//
+//	if len(initials) > 1 {
+//		return initials[0], initials[1]
+//	}
+//
+//	return initials[0], "_"
+//}
+//
+//func main() {
+//	fn1, sn1 := getInitials("tifa lockhart")
+//	fmt.Println(fn1, sn1)
+//
+//	fn2, sn2 := getInitials("cloud strife")
+//	fmt.Println(fn2, sn2)
+//
+//	fn3, sn3 := getInitials("vincent")
+//	fmt.Println(fn3, sn3)
+//}
 
 //func sayGreeting(n string) {
 //	fmt.Printf("Good morning %v \n", n)
